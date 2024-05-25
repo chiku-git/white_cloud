572903724faf8e9ebd83170c9e994c5813b5e23e
unknown <naocosan0808@gmail.com>
2024-05-25 18:59:48 +0900

    [backend] vscode設定の追加とformat

--- backend/api/src/app/account/admin.py
+++ backend/api/src/app/account/admin.py
@@ -1,39 +1,46 @@
 from django.contrib import admin
-
 from .models import User
 
-# Register your models here.
+
 class UserAdmin(admin.ModelAdmin):
     readonly_fields = (
-        'id',
-        'created_at',
-        'updated_at',
+        "id",
+        "created_at",
+        "updated_at",
     )
     fieldsets = [
-        ('基本情報', {
-            'fields': [
-                'id',
-                'username',
-                'email',
-                'bio',
-            ]
-        }),
-        ('システム情報', {
-            'fields': [
-                'last_login_at',
-                'created_at',
-                'updated_at',
-            ]
-        }),
-        ('権限情報', {
-            'fields': [
-                'is_active',
-                'is_superuser',
-                'is_staff',
-            ]
-        }),
+        (
+            "基本情報",
+            {
+                "fields": [
+                    "id",
+                    "username",
+                    "email",
+                    "bio",
+                ]
+            },
+        ),
+        (
+            "システム情報",
+            {
+                "fields": [
+                    "last_login_at",
+                    "created_at",
+                    "updated_at",
+                ]
+            },
+        ),
+        (
+            "権限情報",
+            {
+                "fields": [
+                    "is_active",
+                    "is_superuser",
+                    "is_staff",
+                ]
+            },
+        ),
     ]
-
 
 
 admin.site.register(User, UserAdmin)
