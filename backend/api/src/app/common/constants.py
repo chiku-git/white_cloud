15bf538ea64b2bcbc4d7019ba161d72adb17bacd
unknown <naocosan0808@gmail.com>
2024-06-30 14:26:46 +0900

    [backend]部分修正

--- backend/api/src/app/common/constants.py
+++ backend/api/src/app/common/constants.py
@@ -5,4 +5,4 @@ MAX_SEND_AUTH_CODE_COUNT = 5
 MAX_AUTH_EMAIL_COUNT = 5
 
 # メールアドレス正規表現
-REGEX_EMAIL_ADDRESS = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+REGEX_EMAIL_ADDRESS = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$"
